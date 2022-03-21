6fcdd87b04c423d4fa7c3d1f284a354c75643719
bbibbibiq <peerasorn@hot-now.com>
2022-03-22 01:53:03 +0700

    [WIP] terminal header

--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./index.css";
 
 const Terminal: React.FC = () => {
-  return <div className="terminal">PH</div>;
+  return (
+    <div className="terminal">
+      <div className="terminal-header">
+        <div className="terminal-red-button"></div>
+        <div className="terminal-yellow-button"></div>
+        <div className="terminal-green-button"></div>
+      </div>
+      <div>Peerasorn hemsart</div>
+    </div>
+  );
 };
 
 export default Terminal;
