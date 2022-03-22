54c3fb4e83f718f7a861f392ea6d1c5d74d90dc3
bbibbibiq <peerasorn@hot-now.com>
2022-03-22 22:04:54 +0700

    Updates

--- src/svg/index.tsx
+++
@@ -1,8 +0,0 @@
-import Apple from "./Apple";
-import Battery from "./Battery";
-import Docker from "./Docker";
-import Magnify from "./Magnify";
-import Wifi from "./Wifi";
-import Volume from "./Volume";
-
-export { Apple, Battery, Docker, Magnify, Wifi, Volume };
