08425c27b1215c9c89442f3fe46914b1147c530b
bbibbibiq <peerasorn@hot-now.com>
2022-04-19 18:42:05 +0700

    Dragable

--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -1,19 +1,20 @@
-import React from "react";
 import "./index.css";
 
 import Profile from "./Profile";
 
 import profile from "../../profile.json";
 import useCommand from "../../hooks/useCommand";
+import useDrag from "../../hooks/useDrag";
 
 const DIRECTORY = "~/Peerasorn/Profile";
 
-const Terminal: React.FC = () => {
+const Terminal = () => {
+  const { setRef, initialiseDrag } = useDrag();
   const { command, isEnd } = useCommand(200);
 
   return (
-    <div className="terminal">
-      <div className="terminal-header">
+    <div className="terminal absolute" ref={setRef}>
+      <div className="terminal-header" onMouseDown={initialiseDrag}>
         <div className="terminal-red-button"></div>
         <div className="terminal-yellow-button"></div>
         <div className="terminal-green-button"></div>
---
+++ src/hooks/useDrag.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState, useRef } from "react";
+
+const useDrag = () => {
+  const elemRef = useRef<any>(null);
+  const dragProps = useRef<any>();
+
+  const [ref, setRef] = useState<any>(null);
+
+  useEffect(() => {
+    if (ref) {
+      console.log(ref);
+      dragProps.current = ref;
+      elemRef.current = ref;
+    }
+  }, [ref]);
+
+  const initialiseDrag = (event: any) => {
+    const { target, clientX, clientY } = event;
+    const { offsetTop, offsetLeft } = target;
+    const { left, top } = elemRef.current.getBoundingClientRect();
+
+    dragProps.current = {
+      dragStartLeft: left - offsetLeft,
+      dragStartTop: top - offsetTop,
+      dragStartX: clientX,
+      dragStartY: clientY,
+    };
+
+    window.addEventListener("mousemove", startDragging, false);
+    window.addEventListener("mouseup", stopDragging, false);
+  };
+
+  const startDragging = ({ clientX, clientY }: any) => {
+    // elemRef.current.style.transform = `translate(${
+    //   dragProps.current.dragStartLeft + clientX - dragProps.current.dragStartX
+    // }px, ${
+    //   dragProps.current.dragStartTop + clientY - dragProps.current.dragStartY
+    // }px)`;
+    elemRef.current.style.left = `${
+      dragProps.current.dragStartLeft + clientX - dragProps.current.dragStartX
+    }px`;
+
+    elemRef.current.style.top = `${
+      dragProps.current.dragStartTop + clientY - dragProps.current.dragStartY
+    }px`;
+  };
+
+  const stopDragging = () => {
+    window.removeEventListener("mousemove", startDragging, false);
+    window.removeEventListener("mouseup", stopDragging, false);
+  };
+
+  return { setRef, initialiseDrag };
+};
+
+export default useDrag;
