51efbe4064334e5b9f510e0e6345d08cdd04bdb5
bbibbibiq <peerasorn@hot-now.com>
2022-04-20 17:22:01 +0700

    Handle open/close application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Screen from "./components/Screen";
 import MenuBar from "./components/MenuBar";
 import Terminal from "./components/Terminal";
+import Resume from "./components/Resume";
 import Applications from "./components/Applications";
 import Mobile from "./components/Mobile";
 
@@ -10,8 +11,43 @@ import useWindowDimensions from "./hooks/useWindowDimensions";
 
 import "./App.css";
 
+export type AppType = "resume" | "terminal";
+
 const App: React.FC = () => {
   const { isDesktop } = useWindowDimensions();
+
+  const [appOpened, setAppOpened] = useState<AppType[]>(["terminal"]);
+
+  const onOpen = (appType: AppType) => {
+    const updatedOpenApp: AppType[] = appOpened.includes(appType)
+      ? [...appOpened]
+      : [...appOpened, appType];
+    setAppOpened(updatedOpenApp);
+  };
+
+  const onClose = (appType: AppType) => {
+    const updatedOpenApp: AppType[] = appOpened.filter(
+      (app) => app !== appType
+    );
+    setAppOpened(updatedOpenApp);
+  };
+
+  const renderAppOpened = () => {
+    if (appOpened.length === 0) {
+      return null;
+    }
+
+    return appOpened.map((appType) => {
+      switch (appType) {
+        case "resume":
+          return <Resume key={appType} onClose={onClose} />;
+        case "terminal":
+          return <Terminal key={appType} onClose={onClose} />;
+        default:
+          return null;
+      }
+    });
+  };
 
   return (
     <>
@@ -19,9 +55,9 @@ const App: React.FC = () => {
         <>
           <MenuBar />
           <Screen>
-            <Terminal />
+            <>{renderAppOpened()}</>
 
-            <Applications />
+            <Applications onOpen={onOpen} />
           </Screen>
         </>
       )}
--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -1,26 +1,20 @@
 import type { AppType } from "../../App";
 
-import Profile from "./Profile";
-import profileData from "../../profile.json";
-import useCommand from "../../hooks/useCommand";
 import useDrag from "../../hooks/useDrag";
 
-import "./index.css";
-
 const DIRECTORY = "~/Peerasorn/Profile";
-const APP_NAME = "terminal";
+const APP_NAME = "resume";
 
-interface TerminalProps {
+interface ResumeProps {
   onClose: (appType: AppType) => void;
 }
 
-const Terminal = (props: TerminalProps) => {
+const Resume = (props: ResumeProps) => {
   const { onClose } = props;
   const { setRef, initialiseDrag } = useDrag();
-  const { command, isEnd } = useCommand(200);
 
   return (
-    <div className="terminal absolute z-10 top-1/4 left-1/4" ref={setRef}>
+    <div className="terminal absolute z-10" ref={setRef}>
       <div className="terminal-header" onMouseDown={initialiseDrag}>
         <div
           className="terminal-red-button"
@@ -33,17 +27,11 @@ const Terminal = (props: TerminalProps) => {
         <div className="terminal-prompt">
           <span className="terminal-directory-text">{DIRECTORY}</span>
           <div className="triangle-prompt"></div>
-          <span className="terminal-command typewriter">{command}</span>
         </div>
-        <div
-          className="terminal-output"
-          style={{ visibility: isEnd ? "visible" : "hidden" }}
-        >
-          <Profile profile={profileData} />
-        </div>
+        <div>PDF VIEWER</div>
       </div>
     </div>
   );
 };
 
-export default Terminal;
+export default Resume;
