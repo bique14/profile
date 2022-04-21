6a8a41b960adcc0d9387a6983620c3910a155929
bbibbibiq <peerasorn@hot-now.com>
2022-04-21 18:54:20 +0700

    [WIP] Resume

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -1,9 +1,9 @@
 import type { AppType } from "../../App";
 
+import ApplicationHeaderControl from "../ApplicationHeaderControl";
 import useDrag from "../../hooks/useDrag";
 
-const DIRECTORY = "~/Peerasorn/Profile";
-const APP_NAME = "resume";
+const APP_SLUG = "resume";
 
 interface ResumeProps {
   onClose: (appType: AppType) => void;
@@ -14,21 +14,58 @@ const Resume = (props: ResumeProps) => {
   const { setRef, initialiseDrag } = useDrag();
 
   return (
-    <div className="terminal absolute z-10" ref={setRef}>
-      <div className="terminal-header" onMouseDown={initialiseDrag}>
-        <div
-          className="terminal-red-button"
-          onClick={() => onClose(APP_NAME)}
-        ></div>
-        <div className="terminal-yellow-button"></div>
-        <div className="terminal-green-button"></div>
-      </div>
-      <div className="terminal-body">
-        <div className="terminal-prompt">
-          <span className="terminal-directory-text">{DIRECTORY}</span>
-          <div className="triangle-prompt"></div>
+    <div
+      className="w-1/2 h-4/5 bg-[rgb(234,234,234)] rounded-[4px] absolute z-10"
+      ref={setRef}
+    >
+      <div
+        className="flex px-2 h-5"
+        style={{
+          borderRadius: "4px 4px 0 0",
+        }}
+        onMouseDown={initialiseDrag}
+      >
+        <ApplicationHeaderControl appSlug={APP_SLUG} onClose={onClose} />
+        <div className="flex-1 text-center text-black self-center text-sm cursor-default">
+          Peerasorn Hemsart - resume
         </div>
-        <div>PDF VIEWER</div>
+      </div>
+      <div>Tools bar</div>
+      <div className="bg-[rgb(221,221,221)] h-full">
+        <div
+          id="paper"
+          className="bg-white mx-4 h-full shadow-md flex flex-col px-4 py-10"
+        >
+          <div className="text-center border-b-2 pb-4">
+            <h1 className="font-bold">Peerasorn Hemsart (Big)</h1>
+            <span>Frontend Developer</span>
+          </div>
+          <div id="employment-history" className="mt-4">
+            <span className="font-bold">EMPLOYMENT HISTORY</span>
+
+            <div className="flex flex-col text-sm">
+              <span className="font-bold">
+                SCG, Bangkok - Frontend Developer
+              </span>
+              <span className="text-xs text-gray-500">Sep 2021 - Present</span>
+              <span className="italic">E-commerce website</span>
+              <a
+                className="underline text-blue-700"
+                href="https://www.doozyonline.com/"
+              >
+                https://www.doozyonline.com/
+              </a>
+              <span>
+                An e-commerce website for selling products about packaging of
+                SCG.
+              </span>
+              <span className="text-xs">
+                STACK : ReactJS, Material UI, Styled Component, Magento,
+                GraphQL, Git Hook
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
