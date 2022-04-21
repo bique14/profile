f64165fde24e5ac8900b722a7ae11107886b156f
bbibbibiq <peerasorn@hot-now.com>
2022-04-22 01:37:07 +0700

    Added new icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,10 +1,12 @@
-import PDF from "./pdf-file";
 import Apple from "./apple";
+import Download from "./download";
+import PDF from "./pdf-file";
 import Terminal from "./terminal";
 
 const Icons = {
-  PDF,
   Apple,
+  Download,
+  PDF,
   Terminal,
 };
 
