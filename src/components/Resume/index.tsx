168fa34a7bf2e4f3ea75077cf06cb1b379957d4b
bbibbibiq <peerasorn@hot-now.com>
2022-04-22 01:37:30 +0700

    [WIP] Resume pdf

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -1,7 +1,13 @@
 import type { AppType } from "../../App";
+import type { ResumeType } from "./resume.type";
 
 import ApplicationHeaderControl from "../ApplicationHeaderControl";
 import useDrag from "../../hooks/useDrag";
+
+import Icons from "../../icons";
+import { useRef } from "react";
+
+const RESUME: ResumeType = require("./resume.json");
 
 const APP_SLUG = "resume";
 
@@ -9,9 +15,33 @@ interface ResumeProps {
   onClose: (appType: AppType) => void;
 }
 
+type ZoomType = "zoom-in" | "zoom-out";
+
 const Resume = (props: ResumeProps) => {
   const { onClose } = props;
   const { setRef, initialiseDrag } = useDrag();
+
+  const paperRef = useRef<any>();
+
+  const onClick = (zoomType: ZoomType) => {
+    const currentScale = +(paperRef.current.style.transform || 0).replace(
+      /^\D+|\)/g,
+      ""
+    );
+
+    switch (zoomType) {
+      case "zoom-in":
+        return (paperRef.current.style = `transform: scale(${
+          currentScale + 0.1
+        });`);
+      case "zoom-out":
+        return (paperRef.current.style = `transform: scale(${
+          currentScale - 0.1
+        });`);
+      default:
+        return;
+    }
+  };
 
   return (
     <div
@@ -30,44 +60,193 @@ const Resume = (props: ResumeProps) => {
           Peerasorn Hemsart - resume
         </div>
       </div>
-      <div>Tools bar</div>
-      <div className="bg-[rgb(221,221,221)] h-full">
+      <div className="flex justify-between px-2 mb-1 h-6">
+        <div className="flex gap-1">
+          <div
+            className="bg-white px-2 flex items-center h-full rounded text-sm text-gray-400 cursor-pointer"
+            onClick={() => onClick("zoom-in")}
+          >
+            +
+          </div>
+          <div
+            className="bg-white px-2 flex items-center h-full rounded text-sm text-gray-400 cursor-pointer"
+            onClick={() => onClick("zoom-out")}
+          >
+            -
+          </div>
+          <div className="bg-white rounded self-center px-2 h-full cursor-pointer">
+            <div className="w-4 my-1">
+              <Icons.Download />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center bg-white rounded text-xs text-gray-400 w-40">
+          Search
+        </div>
+      </div>
+      <div className="bg-[rgb(221,221,221)] h-full py-2 px-4 overflow-scroll flex flex-col gap-2">
         <div
-          id="paper"
-          className="bg-white mx-4 h-full shadow-md flex flex-col px-4 py-10"
+          id="paper-1"
+          ref={paperRef}
+          className="bg-white h-fit shadow-md flex flex-col px-4 py-10"
+          style={{ transform: "scale(1)" }}
         >
           <div className="text-center border-b-2 pb-4">
-            <h1 className="font-bold">Peerasorn Hemsart (Big)</h1>
-            <span>Frontend Developer</span>
+            <h1 className="font-bold">
+              {RESUME.name} {RESUME.nickname}
+            </h1>
+            <span>{RESUME.position}</span>
           </div>
-          <div id="employment-history" className="mt-4">
-            <span className="font-bold">EMPLOYMENT HISTORY</span>
+          <div id="my-experienced" className="mt-4">
+            <div className="border-b-2 pb-4">
+              <Companies />
+            </div>
 
-            <div className="flex flex-col text-sm">
-              <span className="font-bold">
-                SCG, Bangkok - Frontend Developer
-              </span>
-              <span className="text-xs text-gray-500">Sep 2021 - Present</span>
-              <span className="italic">E-commerce website</span>
-              <a
-                className="underline text-blue-700"
-                href="https://www.doozyonline.com/"
-              >
-                https://www.doozyonline.com/
-              </a>
-              <span>
-                An e-commerce website for selling products about packaging of
-                SCG.
-              </span>
-              <span className="text-xs">
-                STACK : ReactJS, Material UI, Styled Component, Magento,
-                GraphQL, Git Hook
-              </span>
+            <div className="mt-4 border-b-2 pb-4">
+              <Internship />
+            </div>
+          </div>
+        </div>
+        <div
+          id="paper-2"
+          className="bg-white h-fit shadow-md flex flex-col px-4 py-10"
+        >
+          <div className="mt-4">
+            <div className="border-b-2 pb-4">
+              <Skill />
+            </div>
+
+            <div className="mt-4 border-b-2 pb-4">
+              <Educations />
             </div>
           </div>
         </div>
       </div>
     </div>
+  );
+};
+
+const Companies = () => {
+  const { companies } = RESUME;
+
+  return (
+    <>
+      <span className="font-bold">EMPLOYMENT HISTORY</span>
+      <div className="flex flex-col gap-4 mt-2">
+        {companies.map((company) => (
+          <div key={company.name} className="flex flex-col text-sm">
+            <span className="font-bold">
+              {company.name}, {company.location} - {company.position}
+            </span>
+            <span className="text-xs text-gray-500">
+              {company.duration.start} - {company.duration.end}
+            </span>
+
+            <div className="flex flex-col gap-2">
+              {company.projects.map((project) => (
+                <div
+                  key={`${company.name}-${project.name}`}
+                  className="flex flex-col"
+                >
+                  <span className="italic">{project.name}</span>
+                  {project.link && (
+                    <a
+                      className="underline text-blue-700"
+                      href={project.link}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {project.link}
+                    </a>
+                  )}
+                  <span>{project.description}</span>
+                  <span className="text-xs text-gray-400">
+                    STACKS : {project.stacks}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+const Internship = () => {
+  const { internship } = RESUME;
+
+  return (
+    <>
+      <span className="font-bold">INTERNSHIP</span>
+      <div className="flex flex-col text-sm mt-2">
+        <span className="font-bold">
+          {internship.company}, {internship.location} - {internship.position}
+        </span>
+        <span className="text-xs text-gray-500">
+          {internship.duration.start} - {internship.duration.end}
+        </span>
+        <span className="italic">{internship.project.name}</span>
+        <span>{internship.project.description}</span>
+        <span className="text-xs text-gray-400">
+          STACK : {internship.project.stacks}
+        </span>
+      </div>
+    </>
+  );
+};
+
+const Skill = () => {
+  const { skill } = RESUME;
+
+  return (
+    <>
+      <span className="font-bold">SKILLS</span>
+      <div className="flex flex-col gap-2 text-sm mt-2">
+        <div>
+          <span className="font-bold">LANGUAGES</span>
+          <div className="grid grid-cols-5">
+            {skill.languages.map((language) => (
+              <span key={language} className="text-xs text-gray-500">
+                {language}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div>
+          <span className="text-sm font-bold">TOOLS</span>
+          <div className="grid grid-cols-5">
+            {skill.tools.map((language) => (
+              <span key={language} className="text-xs text-gray-500">
+                {language}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Educations = () => {
+  const { educations } = RESUME;
+
+  return (
+    <>
+      <span className="font-bold">EDUCATION</span>
+      <div className="flex flex-col gap-2 text-sm mt-2">
+        {educations.map((education) => (
+          <div className="flex flex-col">
+            <span>
+              {education.degree}, {education.institution} - {education.major}
+            </span>
+            <span className="text-xs text-gray-500">
+              {education.duration.start} - {education.duration.end}
+            </span>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
