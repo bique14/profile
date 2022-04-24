d376b3b6d6d234ee8bcfd4fa6cd451fc4482780e
bbibbibiq <peerasorn@hot-now.com>
2022-04-24 17:30:01 +0700

    Update terminal styling

--- src/components/Mobile/index.tsx
+++ src/components/Mobile/index.tsx
@@ -1,17 +1,23 @@
+import Terminal from "../Terminal";
 import Icons from "../../icons";
 
 const Mobile = () => {
   return (
-    <div className="h-full flex flex-col justify-center items-cente bg-gray-900">
-      <div className="w-20 self-center">
-        <Icons.Apple />
+    <>
+      <div className="fade-out w-full h-full flex flex-col justify-center items-center bg-gray-900 absolute z-20">
+        <div className="w-20 self-center">
+          <Icons.Apple />
+        </div>
+        <span className="text-center text-white mt-4">
+          You can still read my resume
+          <br />
+          but for better experience
+          <br />
+          please open on the computer
+        </span>
       </div>
-      <span className="text-center text-white mt-4">
-        for better experience
-        <br />
-        please open on the computer
-      </span>
-    </div>
+      <Terminal onClose={() => {}} />
+    </>
   );
 };
 
