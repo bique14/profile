5cb479cc9e1b3c0883e1c28b641a9903c8fe6f52
bbibbibiq <peerasorn@hot-now.com>
2022-04-24 17:08:22 +0700

    Added new icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,10 +1,12 @@
 import Apple from "./apple";
+import ChevronRight from "./chevron.right";
 import Download from "./download";
 import PDF from "./pdf-file";
 import Terminal from "./terminal";
 
 const Icons = {
   Apple,
+  ChevronRight,
   Download,
   PDF,
   Terminal,
