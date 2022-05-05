541390a3f3d6057a83d924a8bada9f9f1ae4b2ce
bbibbibiq <peerasorn@hot-now.com>
2022-05-05 12:33:14 +0700

    Added images

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,12 +3,16 @@ import ChevronRight from "./chevron.right";
 import Download from "./download";
 import PDF from "./pdf-file";
 import Terminal from "./terminal";
+import Note from "./images/note.png";
+import Spotify from "./images/spotify.png";
 
 const Icons = {
   Apple,
   ChevronRight,
   Download,
   PDF,
+  Note,
+  Spotify,
   Terminal,
 };
 
