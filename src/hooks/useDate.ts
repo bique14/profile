5ff13bab7549d6e8035a1cca80969c2012694f94
bbibbibiq <peerasorn@hot-now.com>
2022-12-20 12:54:32 +0700

    refactor: useDate

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -4,25 +4,25 @@ interface IMapping {
   day: (d: number) => string;
   month: (d: number) => string;
 }
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 const mapping: IMapping = {
-  day: (d: number): string =>
-    ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][d],
-  month: (m: number): string =>
-    [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ][m],
+  day: (d: number): string => days[d],
+  month: (m: number): string => months[m],
 };
 
 const useDate = () => {
