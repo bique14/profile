1f9ac925a63a3fef7b312e859f72461ea0ed6bce
bbibbibiq <peerasorn@hot-now.com>
2022-12-20 15:00:50 +0700

    iframe: check when iframe has loaded

--- src/components/Spotify/index.tsx
+++ src/components/Spotify/index.tsx
@@ -1,6 +1,7 @@
 import type { AppType } from "../../types";
 import useDrag from "../../hooks/useDrag";
 import ApplicationHeaderControl from "../ApplicationHeaderControl";
+import { useState } from "react";
 
 const APP_SLUG = "spotify";
 
@@ -12,25 +13,34 @@ const Spotify = (props: SpotifyProps) => {
   const { onClose } = props;
   const { setRef, initialiseDrag } = useDrag();
 
+  const [iframeLoaded, setIFrameLoaded] = useState<boolean>(false);
+
+  const handleIframeLoaded = () => {
+    setIFrameLoaded(true);
+  };
+
   return (
     <div className="w-4/5 h-4/5 absolute top-10 left-40 z-20" ref={setRef}>
-      <div
-        className="flex px-2 h-5 bg-[rgb(18,18,18)]"
-        style={{
-          borderRadius: "4px 4px 0 0",
-        }}
-        onMouseDown={initialiseDrag}
-      >
-        <ApplicationHeaderControl appSlug={APP_SLUG} onClose={onClose} />
-        <div className="flex-1 text-center text-white self-center text-sm cursor-default">
-          Spotify
+      {iframeLoaded && (
+        <div
+          className="flex px-2 h-5 bg-[rgb(18,18,18)]"
+          style={{
+            borderRadius: "4px 4px 0 0",
+          }}
+          onMouseDown={initialiseDrag}
+        >
+          <ApplicationHeaderControl appSlug={APP_SLUG} onClose={onClose} />
+          <div className="flex-1 text-center text-white self-center text-sm cursor-default">
+            Spotify
+          </div>
         </div>
-      </div>
+      )}
 
       <iframe
         className="w-full h-full"
         src="https://spotify-fake-lyric.netlify.app/"
         title="Spotify"
+        onLoad={handleIframeLoaded}
       />
     </div>
   );
