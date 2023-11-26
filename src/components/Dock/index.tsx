ec14b78892cd7a0b89ef9b4511812e8d6839e564
bique14 <peerasorn.h@kbtg.tech>
2023-11-26 12:22:25 +0700

    fix: update styling

--- src/components/Dock/index.tsx
+++ src/components/Dock/index.tsx
@@ -7,7 +7,7 @@ const Dock = (props: DockProps) => {
 
   return (
     <div
-      className="bg-[rgba(98,98,98,0.7)] absolute z-20 bottom-0 left-1/2 rounded-t flex"
+      className="bg-[rgba(98,98,98,0.7)] absolute z-30 bottom-0 left-1/2 rounded-t flex"
       style={{ transform: "translate(-50%,0%)" }}
     >
       {children}
